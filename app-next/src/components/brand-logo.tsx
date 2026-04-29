e269a54dbabb1bc0038844f5eda952e3306d733d
Shishir <shishir.svyas@gmail.com>
2026-04-29 09:05:10 +0530

    Handling MObile version

--- app-next/src/components/brand-logo.tsx
+++ app-next/src/components/brand-logo.tsx
@@ -8,11 +8,12 @@ type BrandLogoProps = {
 
 export function BrandLogo({ className, invert = false }: BrandLogoProps) {
   return (
-    <div className={cn("flex items-center", className)}>
+    <div className={cn("flex shrink-0 items-center", className)}>
       {/* eslint-disable-next-line @next/next/no-img-element */}
       <img
         src="/brand/lan-logo.png"
         alt="LAN Engineering & Technologies"
+        className="block h-9 w-auto max-w-[140px] shrink-0 object-contain"
         style={{ height: 36, width: "auto", filter: invert ? "brightness(0) invert(1)" : undefined }}
       />
     </div>
